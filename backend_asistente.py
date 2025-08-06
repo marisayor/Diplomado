f54bd78ba0a0a8cf2c7e9b308b57157c6dac6775
marisayor <39377819+marisayor@users.noreply.github.com>
2025-08-06 08:33:07 -0400

    Update backend_asistente.py

--- backend_asistente.py
+++ backend_asistente.py
@@ -205,4 +205,3 @@ if __name__ == '__main__':
     # Render usa Gunicorn, por lo que este bloque no se ejecuta en producción.
     # El puerto 10000 es el que usa Render, no 5000.
     app.run(debug=False, host='0.0.0.0', port=os.environ.get('PORT', 5000))
-
