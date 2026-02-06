0f95eee074828d65e3ab0728e190b8080bda1ae6
marisayor <39377819+marisayor@users.noreply.github.com>
2026-02-06 13:52:47 -0400

    Update backend_asistente.py

--- backend_asistente.py
+++ backend_asistente.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_community.vectorstores import Chroma
-from langchain.chains.retrieval_qa.base import RetrievalQA  # ✅ IMPORT CORREGIDO (línea 6)
+from from langchain.chains.retrieval_qa.base import RetrievalQA
 from langchain.prompts import PromptTemplate
 import os
 from langchain_community.document_loaders import PyPDFLoader
