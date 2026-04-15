c547f7dbd74fb38f5a4ec4f800ffda4f5a7c01c9
marisayor <39377819+marisayor@users.noreply.github.com>
2026-04-15 08:01:37 -0400

    Update backend_asistente.py

--- backend_asistente.py
+++ backend_asistente.py
@@ -48,7 +48,7 @@ def background_setup():
         PERSIST_DIRECTORY = "./chroma_db_diabetes"
 
         embeddings_model = GoogleGenerativeAIEmbeddings(
-    model="text-embedding-003",
+    model="models/text-embedding-004",
     google_api_key=API_KEY
         )
 
