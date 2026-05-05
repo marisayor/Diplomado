158baf9e2f4239bbc7a524ab6877398bee3ed82e
marisayor <39377819+marisayor@users.noreply.github.com>
2026-05-05 08:18:22 -0400

    Update backend_asistente.py

--- backend_asistente.py
+++ backend_asistente.py
@@ -51,7 +51,7 @@ def background_setup():
 
         # Inicialización de Embeddings (Modelo estable v4)
         embeddings_model = GoogleGenerativeAIEmbeddings(
-            model="text-embedding-004", 
+            model="text-embedding-001", 
             google_api_key=API_KEY
         )
 
