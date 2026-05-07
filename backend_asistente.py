9838b7ea74cc5be2ed334954edb9e1bbef372356
marisayor <39377819+marisayor@users.noreply.github.com>
2026-05-07 09:41:43 -0400

    Update backend_asistente.py

--- backend_asistente.py
+++ backend_asistente.py
@@ -10,8 +10,8 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import Chroma
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
-from langchain.chains import RetrievalQA
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate # Ruta corregida
+from langchain.chains.retrieval_qa.base import RetrievalQA # Ruta específica
 
 # --- Configuración de Flask ---
 app = Flask(__name__)
