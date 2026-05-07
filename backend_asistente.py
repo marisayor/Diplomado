64d8b10df6ff920794c564614f856cb65fec32f4
marisayor <39377819+marisayor@users.noreply.github.com>
2026-05-07 10:55:43 -0400

    Update backend_asistente.py

--- backend_asistente.py
+++ backend_asistente.py
@@ -11,7 +11,7 @@ from langchain_community.vectorstores import Chroma
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_core.prompts import PromptTemplate # Ruta corregida
-from langchain.chains.retrieval_qa.base import RetrievalQA # Ruta específica
+from langchain_community.chains import RetrievalQA # Ruta específica
 
 # --- Configuración de Flask ---
 app = Flask(__name__)
